17ad75a4a3fdc8948c7ebd5694f35a6f87695788
Shay <moeshakasanova@gmail.com>
2024-03-16 20:42:45 +0400

    app.test fix gh-pages added

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const homeLinks = screen.getAllByText(/Home/i);
+  expect(homeLinks.length).toBeGreaterThan(0);
 });
